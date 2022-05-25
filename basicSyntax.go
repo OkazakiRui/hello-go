075926a65d6c870d78104cd236c08adc3364c03d
RuiOkazaki <okazaki200226@gmail.com>
2022-05-25 14:04:44 +0900

    feat: iota

--- basicSyntax.go
+++ basicSyntax.go
@@ -1,7 +1,6 @@
 package main
 
-
-func s_var(){
+func s_var() {
 	var n int = 100
 	// 自動推論
 	var m = 40
@@ -14,17 +13,38 @@ func s_var(){
 	println(n, m, a, b, c)
 }
 
-func s_const(){
+func s_const() {
 	const n int = 100
 	const m = 40
 	const (
-		b = 20
-		c = 30
+		// 同じ値の場合は右辺を省略することができる
+		a = 20
+		b
+		c
 	)
-	println(n, m, b, c)
+	println(n, m, a, b, c)
+}
+
+func s_iota()  {
+	const (
+		// 連番を付与する
+		a = iota
+		b
+		c
+	)
+	println(a, b, c)
+
+	const (
+		// 左シフトさせる
+		d = 1 << iota
+		e
+		f
+	)
+	println(d, e, f)
 }
 
 func main() {
 	// s_var()
-	s_const()
+	// s_const()
+	s_iota()
 }
